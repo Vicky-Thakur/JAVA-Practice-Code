44d6305b328929526a67904efa5f43fbeb7e6d4a
Vicky Thakur <Vicky Thakur@192.168.1.117>
2018-07-03 10:42:45 +0530

    update

---
+++ Sample-Code/src.java.main/com/practice/FindMax1Row.java
@@ -0,0 +1,19 @@
+package com.practice;
+
+//Row wise Sorted matrix having only 0s, 1s
+//Find Row having maximum number of 1s
+public class FindMax1Row {
+
+	public int findRow(int matrix[][]){
+		
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		int[][] matrix = new int[][]{{0,0,1,1},{0,0,0,0},{0,1,1,1},{0,0,0,1}};
+		FindMax1Row f = new FindMax1Row();
+		int result = f.findRow(matrix);
+		System.out.println("Row having max no of 1s " + result);
+	}
+
+}
