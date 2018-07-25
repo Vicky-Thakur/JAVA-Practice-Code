cd17191b50f559f484de0cb6cfda0f61db9335b4
Vicky Thakur <Vicky Thakur@192.168.1.136>
2018-07-25 09:26:08 +0530

    Iterative Tree traversal

--- Sample-Code/src.java.main/com/practice/bt/TreeTraversalIterative.java
+++ Sample-Code/src.java.main/com/practice/bt/TreeTraversalIterative.java
@@ -8,39 +8,72 @@ public class TreeTraversalIterative {
 		if(temp==null)
 			return;
 		Stack<Node> stack = new Stack<Node>();
-		stack.push(temp);
-		while(!stack.isEmpty()){
+		while(!stack.isEmpty() || temp!=null){
 			if(temp!=null){
 				stack.push(temp);
 				temp = temp.l;
 			}else{
 				temp = stack.pop();
-				System.out.println(temp.value + ",");
+				System.out.print(temp.value + ",");
 				temp = temp.r;
 			}
 		}
 	}
 	
 	public void preorder(Node temp){
-		
+		Stack<Node> stack = new Stack<Node>();
+		while(!stack.isEmpty() || temp!=null){
+			if(temp!=null){
+				System.out.print(temp.value+",");
+				if(temp.r!=null)
+					stack.push(temp.r);
+				temp = temp.l;
+			}else{
+				temp = stack.pop();
+			}
+		}
 	}
 
 	public void postorder(Node temp){
-		
+		if(temp==null)
+			return;
+		Stack<Node> stack = new Stack<Node>();
+		//first time stack is empty So temp!=null condition is required otherwise not required
+		while(!stack.isEmpty() || temp!=null){
+			if(temp!=null){
+				//push temp as well as right also
+				if(temp.r!=null)
+					stack.push(temp.r);
+				stack.push(temp);
+				temp=temp.l;
+			}else{
+				temp = stack.pop();
+				Node peek = stack.isEmpty() ? null : stack.peek();
+				//if current node's right is same as next to stack top(stack.peek()).
+				//	then current node's right is not traversed yet
+				if(temp.r!=null && temp.r==peek){
+					stack.pop();
+					stack.push(temp);
+					temp = temp.r;
+				}else{ //else current node's right is either traversed or null
+					System.out.print(temp.value+",");
+					temp = null;
+				}
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
 		BinaryTree bt = new BinaryTree();
-		for(int i=1;i<15;i++){
+		for(int i=1;i<9;i++){
 			bt.insert(i);
 		}
-		System.out.println("Your inserted tree is :");
 		TreeTraversalIterative ttI = new TreeTraversalIterative();
 		System.out.println("Iterative Inorder traversal of tree is :");
 		ttI.inorder(bt.root);
-		System.out.println("Iterative Preorder traversal of tree is :");
+		System.out.println("\nIterative Preorder traversal of tree is :");
 		ttI.preorder(bt.root);
-		System.out.println("Iterative Postorder traversal of tree is :");
+		System.out.println("\nIterative Postorder traversal of tree is :");
 		ttI.postorder(bt.root);
 	}
 
