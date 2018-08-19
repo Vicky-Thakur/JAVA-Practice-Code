6c586d4edf3135565b2e3d848d496aca20703572
Vicky <Vicky@hp>
2018-08-19 12:28:52 +0530

    print all anagram together

--- Sample-Code/src.java.main/com/practice/string/WordsAnagram.java
+++ Sample-Code/src.java.main/com/practice/string/WordsAnagram.java
@@ -28,26 +28,51 @@ public class WordsAnagram {
 	//Approach 2
 	public void wordAnagram(String words[]){
 		int[] index = new int[words.length];
-		StringBuilder[] wordsCopy = new StringBuilder[words.length];
+		String[] wordsCopy = new String[words.length];
 		for(int i=0;i<words.length;i++){
-			wordsCopy[i] = new StringBuilder(words[i]);
+			wordsCopy[i] = wordSort(words[i]);
 			index[i] = i;
 		}
 		
+		for(int i=0;i<wordsCopy.length;i++){
+			int position = i;
+			for(int j=i;j<wordsCopy.length;j++){
+				if(wordsCopy[j].compareTo(wordsCopy[i])<0)
+					position = j;
+			}
+			String temp = wordsCopy[position];
+			wordsCopy[position] = wordsCopy[i];
+			wordsCopy[i] = temp; 
+			int tempInt = index[position];
+			index[position] = index[i];
+			index[i] = tempInt;
+		}
+		
+		for(int k=0;k<words.length;k++){
+			System.out.print(words[index[k]]+", ");
+		}
 	}
 	
+	//assuming word contain only small letters
 	public String wordSort(String word){
 		int alphaAscii[] = new int[26];
 		for(int i = 0;i<word.length();i++){
-			alphaAscii[word.charAt(i)] = 1; 
+			alphaAscii[word.charAt(i)-97] = alphaAscii[word.charAt(i)-97] + 1; 
 		}
 		StringBuilder sortedWord = new StringBuilder();
-		for(int i =0;i<26;i)
+		for(int i =0;i<26;i++){
+			if(alphaAscii[i]>0){
+				for(int j = 1;j<=alphaAscii[i];j++)
+					sortedWord.append((char)(97+i));
+			}
+		}
+		return sortedWord.toString();
 	}
 	
 	public static void main(String[] args) {
 		String wordArr[] = {"dog","hear","earh","god"};
-		
+		WordsAnagram obj = new WordsAnagram();
+		obj.wordAnagram(wordArr);
 	}
 
 }
